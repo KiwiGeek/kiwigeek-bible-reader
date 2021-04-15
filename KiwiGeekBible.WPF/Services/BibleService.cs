fa570ce4734d44e47cdd6bf34e53933e2f056412
Joshua Penman <jpenman@lcg.org>
2021-04-15 11:41:16 -0400

    Remove using statement on removed class

--- KiwiGeekBible.WPF/Services/BibleService.cs
+++ KiwiGeekBible.WPF/Services/BibleService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Windows.Documents;
-using KiwiGeekBible.WPF.Classes;
 using Microsoft.Data.Sqlite;
 
 namespace KiwiGeekBible.WPF.Services
