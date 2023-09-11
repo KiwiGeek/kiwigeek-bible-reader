b44842684245e4fe97ab68e935a0028f87ef19e8
Joshua Penman <joshua@penman.dev>
2023-09-11 15:01:12 -0700

    Quick UI Improvements

--- KiwiGeekBible.WPF/App.xaml.cs
+++ KiwiGeekBible.WPF/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using Telerik.Windows.Controls;
 
 namespace KiwiGeekBible.WPF
@@ -16,7 +11,8 @@ namespace KiwiGeekBible.WPF
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            StyleManager.ApplicationTheme = new Office2019Theme( Office2019Palette.ColorVariation.Gray);
+
+            StyleManager.ApplicationTheme = new Office2019Theme( Office2019Palette.ColorVariation.Gray  );
             MainWindow window = new MainWindow();
             window.Show();
         }
